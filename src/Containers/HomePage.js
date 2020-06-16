8881c6c70619b51afda4d14f5a83a29e6fe2d1ad
huynonstop <tthuykh99@gmail.com>
2020-06-16 12:45:38 +0700

    first commit

---
+++ src/Containers/HomePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import DirectoryMenu from "../Components/DirectoryMenu";
+import "./HomePage.styles.scss";
+
+export default function HomePage() {
+	return (
+		<div className="homepage">
+			<DirectoryMenu></DirectoryMenu>
+		</div>
+	);
+}
